ba81c57710a8a2dfc37dee690ac03240f8043dc6
Walala <conyclopez2@gmail.com>
2024-10-29 23:08:54 -0300

    Update mainset.py

--- mainset.py
+++ mainset.py
@@ -1,14 +1,14 @@
 import streamlit as st
 import requests
-from pags import homepage, pagina2, pagina3, pagina4
+from pags import homepage, pag2, pag3, pag4
 
 elegir = st.sidebar.selectbox('Ir a:',['Inicio','Álbumes','Análisis','Recomendaciones'])
 
 if elegir == 'Inicio':
   homepage.pginicio()
 elif elegir == 'Albumes':
-  pagina2.albumes()
+  pag2.albumes()
 elif elegir == 'Análisis':
-  pagina3.analisis()
+  pag3.analisis()
 elif elegir == 'Recomendaciones':
-  pagina4.recs()
+  pag4.recs()
