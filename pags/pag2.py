9419991c19de50718e8a9dcb061afe3d02c5e782
Walala <conyclopez2@gmail.com>
2024-10-29 23:35:00 -0300

    Update pag2.py

--- pags/pag2.py
+++ pags/pag2.py
@@ -8,4 +8,7 @@ def albumes():
   alb = st.selectbox('Elige un álbum para ver sus canciones y videos musicales disponibles',['Taylor Swift','Fearless','Speak Now','Red','1989','Reputation','Lover','Folklore','Evermore','Midnights','The Tortured Poets Department'])
                                                                                      
   if alb == 'Taylor Swift':
+    st.image('logos/debut.png')
+  else:
+    st.write('si')
   
