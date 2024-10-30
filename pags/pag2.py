3d176b8d22e6a17ed11bb0c984f1caaa31c21444
Walala <conyclopez2@gmail.com>
2024-10-29 23:43:06 -0300

    Update pag2.py

--- pags/pag2.py
+++ pags/pag2.py
@@ -2,8 +2,6 @@ import streamlit as st
 import pandas as pd
 
 def albums():
-  df = pd.read_csv('taylor_swift_spotify.csv')
-
   st.title('Álbumes')
   alb = st.selectbox('Elige un álbum:',['Taylor Swift','Fearless','Speak Now','Red','1989','Reputation','Lover','Folklore','Evermore','Midnights','The Tortured Poets Department'])
                                                                                      
