5d8a3712e88ecb87ae4c6efc034595165d377e6a
Walala <conyclopez2@gmail.com>
2024-10-29 22:27:33 -0300

    Update mainset.py

--- mainset.py
+++ mainset.py
@@ -4,17 +4,21 @@ import requests
 paginas = {
   'Inicio' : 'p1',
   'Albumes' : 'p2',
-  'Análisis' : 'p3'
+  'Análisis' : 'p3',
+  'Recomendaciones' : 'p4'
 }
 
 elegir = st.sidebar.selectbox('Ir a:',list(paginas.keys()))
 
 if elegir == 'Inicio':
-    import secciones.pagina1
-    secciones.pagina1.inicio()
+  import secciones.pagina1
+  secciones.pagina1.inicio()
 elif elegir == 'Albumes':
-    import secciones.pagina2
-    secciones.pagina2.albumes()
+  import secciones.pagina2
+  secciones.pagina2.albumes()
 elif elegir == 'Análisis':
-    import secciones.pagina3
-    secciones.pagina3.analisis()
+  import secciones.pagina3
+  secciones.pagina3.analisis()
+elif elegir == 'Recomendaciones':
+  import secciones.pagina4
+  secciones.pagina4.recs()
