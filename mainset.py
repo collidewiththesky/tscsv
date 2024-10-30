efa19de8ff945e08515276e22443c3c5743207e6
Walala <conyclopez2@gmail.com>
2024-10-29 22:25:09 -0300

    Update mainset.py

--- mainset.py
+++ mainset.py
@@ -1,1 +1,20 @@
+import streamlit as st
+import requests
 
+paginas = {
+  'Inicio' : 'p1',
+  'Albumes' : 'p2',
+  'Análisis' : 'p3'
+}
+
+elegir = st.sidebar.selectbox('Ir a:',list(paginas.keys()))
+
+if elegir == 'Inicio':
+    import secciones.pagina1
+    secciones.pagina1.inicio()
+elif elegir == 'Albumes':
+    import secciones.pagina2
+    secciones.pagina2.albumes()
+elif elegir == 'Análisis':
+    import secciones.pagina3
+    secciones.pagina3.analisis()
