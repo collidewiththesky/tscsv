3d64a12ba9db7f8c2cde11432c96a4bcecf42a26
Walala <conyclopez2@gmail.com>
2024-10-29 23:44:31 -0300

    Update pag2.py

--- pags/pag2.py
+++ pags/pag2.py
@@ -3,7 +3,10 @@ import pandas as pd
 
 def albums():
   st.title('Álbumes')
-  alb = st.selectbox('Elige un álbum:',['Taylor Swift','Fearless','Speak Now','Red','1989','Reputation','Lover','Folklore','Evermore','Midnights','The Tortured Poets Department'])
+  alb = st.selectbox(
+    'Elige un álbum:',
+    ('Taylor Swift','Fearless','Speak Now','Red','1989','Reputation','Lover','Folklore','Evermore','Midnights','The Tortured Poets Department'),
+  )
                                                                                      
   if alb == 'Taylor Swift':
     st.image('logos/debut.png')
