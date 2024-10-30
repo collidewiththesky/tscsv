ca7e5b2e78de3b52a0b72895ad95d5168a995f1c
Walala <conyclopez2@gmail.com>
2024-10-29 23:52:21 -0300

    Update pag2.py

--- pags/pag2.py
+++ pags/pag2.py
@@ -4,17 +4,17 @@ import logos
 
 def albums():
   st.header('Álbumes')
-  alb = st.selectbox(
-    'Elige un álbum:',
-    ('Taylor Swift','Fearless','Speak Now','Red','1989','Reputation','Lover','Folklore','Evermore','Midnights','The Tortured Poets Department'),
-  )
+  #alb = st.selectbox(
+   # 'Elige un álbum:',
+    #('Taylor Swift','Fearless','Speak Now','Red','1989','Reputation','Lover','Folklore','Evermore','Midnights','The Tortured Poets Department'),
+  #)
                                                                                      
-  if alb == 'Taylor Swift':
-    st.write('Elegiste:')
-    st.image('logos/debut.png')
-  elif alb == 'Fearless':
-    st.image('logos/fearless.png')
-  else:
-    st.write('si')
+  #if alb == 'Taylor Swift':
+   # st.write('Elegiste:')
+    #st.image('logos/debut.png')
+  #elif alb == 'Fearless':
+   # st.image('logos/fearless.png')
+  #else:
+  #  st.write('si')
     
   
