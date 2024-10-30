4fbe8a45410daa2b024d510d55867ad41f4a32d8
Walala <conyclopez2@gmail.com>
2024-10-29 23:10:08 -0300

    Update homepage.py

--- pags/homepage.py
+++ pags/homepage.py
@@ -4,6 +4,5 @@ import requests
 def pginicio():
   st.title('Taylor Swift')
   st.write('Álbumes, análisis de Spotify y recomendaciones de canciones')
-  st.markdown('![Taylor Swift en el Eras Tour](https://64.media.tumblr.com/816f0834b6114424000d9b0ea5d1e541/36575a8dabee0ec2-cc/s540x810/0cad0d55b358f3c3069e5cf8626829dd38c73074.gifv'))
+  #st.markdown('![Taylor Swift en el Eras Tour](https://64.media.tumblr.com/816f0834b6114424000d9b0ea5d1e541/36575a8dabee0ec2-cc/s540x810/0cad0d55b358f3c3069e5cf8626829dd38c73074.gifv'))
   st.link_button('Página oficial de Taylor Swift','https://www.taylorswift.com/')
-  except ImportError as e:
