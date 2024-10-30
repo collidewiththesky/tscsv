af30943a9882dc38759477020c7b59b2b50b5273
Walala <conyclopez2@gmail.com>
2024-10-29 23:33:50 -0300

    Update pag2.py

--- pags/pag2.py
+++ pags/pag2.py
@@ -1,8 +1,11 @@
 import streamlit as st
 import pandas as pd
 
-df = pd.read_csv('taylor_swift_spotify.csv')
+def albumes():
+  df = pd.read_csv('taylor_swift_spotify.csv')
 
-st.title('Álbumes')
-alb = st.selectbox('Elige un álbum para ver sus canciones y videos musicales disponibles',['Taylor Swift','Fearless','Speak Now','Red','1989','Reputation','Lover','Folklore','Evermore','Midnights','The Tortured Poets Department'])
+  st.title('Álbumes')
+  alb = st.selectbox('Elige un álbum para ver sus canciones y videos musicales disponibles',['Taylor Swift','Fearless','Speak Now','Red','1989','Reputation','Lover','Folklore','Evermore','Midnights','The Tortured Poets Department'])
                                                                                      
+  if alb == 'Taylor Swift':
+  
