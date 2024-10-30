9578ca4519827a1d6cfe1f6bb3cc8951376f07a0
Walala <conyclopez2@gmail.com>
2024-10-29 23:09:49 -0300

    Update homepage.py

--- pags/homepage.py
+++ pags/homepage.py
@@ -2,12 +2,8 @@ import streamlit as st
 import requests
 
 def pginicio():
-  try:
-    st.title('Taylor Swift')
-    st.subheader('Álbumes, análisis de Spotify y recomendaciones de canciones')
-    st.markdown('![Taylor Swift en el Eras Tour](https://64.media.tumblr.com/816f0834b6114424000d9b0ea5d1e541/36575a8dabee0ec2-cc/s540x810/0cad0d55b358f3c3069e5cf8626829dd38c73074.gifv'))
-    st.link_button('Página oficial de Taylor Swift','https://www.taylorswift.com/')
+  st.title('Taylor Swift')
+  st.write('Álbumes, análisis de Spotify y recomendaciones de canciones')
+  st.markdown('![Taylor Swift en el Eras Tour](https://64.media.tumblr.com/816f0834b6114424000d9b0ea5d1e541/36575a8dabee0ec2-cc/s540x810/0cad0d55b358f3c3069e5cf8626829dd38c73074.gifv'))
+  st.link_button('Página oficial de Taylor Swift','https://www.taylorswift.com/')
   except ImportError as e:
-    st.error(f"Error al importar: {e}")
-  except AttributeError as e:
-    st.error(f"Error al llamar a la función: {e}")
