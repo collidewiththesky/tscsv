c2a77e0fb3e7301f09752e3bdaabdbdb45fef308
Walala <conyclopez2@gmail.com>
2024-10-29 23:32:42 -0300

    Update pag2.py

--- pags/pag2.py
+++ pags/pag2.py
@@ -4,5 +4,5 @@ import pandas as pd
 df = pd.read_csv('taylor_swift_spotify.csv')
 
 st.title('Álbumes')
-alb = st.selectbox('Elige un álbum para ver sus canciones y videos musicales disponibles',['Taylor Swift','Fearless','Speak Now','Red','1989','Reputation','Lover','Folklore','Evermore','Midnights','The Tortured Poets Department])
+alb = st.selectbox('Elige un álbum para ver sus canciones y videos musicales disponibles',['Taylor Swift','Fearless','Speak Now','Red','1989','Reputation','Lover','Folklore','Evermore','Midnights','The Tortured Poets Department'])
                                                                                      
