aa5736566da0454c462fe3b842b90ad8ec166703
Walala <conyclopez2@gmail.com>
2024-10-29 23:08:33 -0300

    Update mainset.py

--- mainset.py
+++ mainset.py
@@ -5,7 +5,7 @@ from pags import homepage, pagina2, pagina3, pagina4
 elegir = st.sidebar.selectbox('Ir a:',['Inicio','Álbumes','Análisis','Recomendaciones'])
 
 if elegir == 'Inicio':
-  homepage.home()
+  homepage.pginicio()
 elif elegir == 'Albumes':
   pagina2.albumes()
 elif elegir == 'Análisis':
