97b88723b2063c48ef6a9aafb2fee1d12ea547a5
Walala <conyclopez2@gmail.com>
2024-11-01 20:52:04 -0300

    Update pag3.py

--- pags/pag3.py
+++ pags/pag3.py
@@ -1,1 +1,11 @@
+import matplotlib.pyplot as plt
+import pandas as pd
 
+def graficos():
+  df = pd.read_csv('taylor_swift_spotify.csv')
+  st.title('Número de Canciones por Álbum')
+  cpa = df['album'].value_counts()
+  colores = ['yellow','red','green','pink','hotpink','khaki','blue','lightblue','orange','fuchsia']
+  fig, ax = plt.subplots()
+  ax.pie(album_counts, labels=album_counts.index, autopct='%1.1f%%', startangle=45)
+  st.pyplot(fig)
