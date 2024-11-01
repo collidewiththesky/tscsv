0ecd19a81192ee646a0c14fc06c1e9ce3a82e28e
Walala <conyclopez2@gmail.com>
2024-11-01 20:05:09 -0300

    Update mainset.py

--- mainset.py
+++ mainset.py
@@ -2,7 +2,7 @@ import streamlit as st
 import requests
 from pags import homepage, pag2, pag3, pag4
 
-elegir = st.sidebar.selectbox('Ir a:',['Inicio','Álbumes','Análisis','Recomendaciones'])
+elegir = st.sidebar.slider('Ir a:',['Inicio','Álbumes','Análisis','Recomendaciones'])
 st.sidebar.markdown('![Alt Text](https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExdW92a242d3AxdHZyOHpwejR1Y21mb29xMWVxdThscjNmYW42d2E5OSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/KBbeJb7fvMnlJsfYWY/giphy.gif)')
 
 if elegir == 'Inicio':
