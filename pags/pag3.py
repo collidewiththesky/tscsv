aa312d02674f769828be57e70f8c98e3a316c1a4
Walala <conyclopez2@gmail.com>
2024-11-01 20:53:11 -0300

    Update pag3.py

--- pags/pag3.py
+++ pags/pag3.py
@@ -7,5 +7,5 @@ def analisis():
   cpa = df['album'].value_counts()
   colores = ['yellow','red','green','pink','hotpink','khaki','blue','lightblue','orange','fuchsia']
   fig, ax = plt.subplots()
-  ax.pie(album_counts, labels=album_counts.index, autopct='%1.1f%%', startangle=45)
+  ax.pie(cpa, labels=cpa.index, autopct='%1.1f%%', startangle=45,colors=colores)
   st.pyplot(fig)
