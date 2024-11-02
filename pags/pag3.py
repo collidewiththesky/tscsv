97251e89812433817a4b437873a73a1ee6c28f97
Walala <conyclopez2@gmail.com>
2024-11-01 21:32:04 -0300

    Update pag3.py

--- pags/pag3.py
+++ pags/pag3.py
@@ -4,34 +4,51 @@ import streamlit as st
 from streamlit_echarts import st_echarts
 
 def analisis():
-  df = pd.read_csv('taylor_swift_spotify.csv')
-  albs = ["Taylor Swift (Deluxe Edition)","Fearless (Taylor's Version)","Speak Now (Taylor's Version)", "Red (Taylor's Version","1989 (Taylor's Version) [Deluxe]","reputation","Lover","folklore (deluxe version)","evermore (deluxe version)","Midnights (The Til Dawn Edition)","THE TORTURED POETS DEPARTMENT: THE ANTHOLOGY"]
-  ab = df[df['album'].isin(albs)]
-  cpa = ab['album'].value_counts().reset_index()
-  cpa.columns = ['album', 'canciones']
-  colores = ['yellow','red','green','lightskyblue','mediumpurple','hotpink','peru','silver','dimgray','limegreen']
-  st.title('Porcentaje de Canciones por Álbumes')
-
-  options = {
-      "title": {"text": "Canciones por Álbums", "left": "center"},
-      "tooltip": {"trigger": "item"},
-      "legend": {"orient": "vertical", "left": "left",},
+  data = pd.read_csv('taylor_swift_spotify.csv')
+  
+  # Definir los álbumes que deseas incluir
+  albumes_seleccionados = ["Taylor Swift (Deluxe Edition)","Fearless (Taylor's Version)","Speak Now (Taylor's Version)", "Red (Taylor's Version","1989 (Taylor's Version) [Deluxe]","reputation","Lover","folklore (deluxe version)","evermore (deluxe version)","Midnights (The Til Dawn Edition)","THE TORTURED POETS DEPARTMENT: THE ANTHOLOGY"]
+  
+  # Filtrar el DataFrame para incluir solo los álbumes seleccionados
+  data_filtrada = data[data['album'].isin(albumes_seleccionados)]
+  
+  # Contar el número de canciones por álbum
+  album_counts = data_filtrada['album'].value_counts().reset_index()
+  album_counts.columns = ['album', 'canciones']
+  
+  # Configurar la página
+  st.title('Número de Canciones por Álbumes Seleccionados de Taylor Swift')
+  
+  # Crear el gráfico de torta con st_echarts
+  option = {
+      "title": {
+          "text": "Número de Canciones por Álbum",
+          "subtext": "Albumes Seleccionados",
+          "left": "center"
+      },
+      "tooltip": {
+          "trigger": "item"
+      },
+      "legend": {
+          "orient": "vertical",
+          "left": "left"
+      },
       "series": [
           {
-              "name": "Canciones",
+              "name": "Número de Canciones",
               "type": "pie",
               "radius": "50%",
-              "data": cpa.to_dict(orient='records'),
+              "data": album_counts.to_dict(orient='records'),
               "emphasis": {
                   "itemStyle": {
                       "shadowBlur": 10,
                       "shadowOffsetX": 0,
                       "shadowColor": "rgba(0, 0, 0, 0.5)"
                   }
-              },
+              }
           }
-      ],
+      ]
   }
-  st_echarts(
-        options=options, height="600px",
-    )
+  
+  # Mostrar el gráfico en Streamlit
+  st_echarts(option=option)
