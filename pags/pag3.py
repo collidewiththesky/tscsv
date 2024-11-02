f07dd3110776b9f94e072b3962061bb19e7bcbb8
Walala <conyclopez2@gmail.com>
2024-11-01 21:16:26 -0300

    Update pag3.py

--- pags/pag3.py
+++ pags/pag3.py
@@ -1,7 +1,6 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import streamlit as st
-from streamlit_echarts import st_echarts
 
 def analisis():
   df = pd.read_csv('taylor_swift_spotify.csv')
