01c247a47cd9849c833f6705d6a21540c2b022e1
Walala <conyclopez2@gmail.com>
2024-11-01 21:15:56 -0300

    Update pag3.py

--- pags/pag3.py
+++ pags/pag3.py
@@ -4,7 +4,6 @@ import streamlit as st
 from streamlit_echarts import st_echarts
 
 def analisis():
-  st.
   df = pd.read_csv('taylor_swift_spotify.csv')
   albs = ["Taylor Swift (Deluxe Edition)","Fearless (Taylor's Version)","Speak Now (Taylor's Version)", "Red (Taylor's Version","1989 (Taylor's Version) [Deluxe]","reputation","Lover","folklore (deluxe version)","evermore (deluxe version)","Midnights (The Til Dawn Edition)","THE TORTURED POETS DEPARTMENT: THE ANTHOLOGY"]
   ab = df[df['album'].isin(albs)]
