8cc125fa8444d22557bb003c084b46872eb416da
Walala <conyclopez2@gmail.com>
2024-11-01 21:56:44 -0300

    Update pag3.py

--- pags/pag3.py
+++ pags/pag3.py
@@ -5,22 +5,22 @@ from streamlit_echarts import st_echarts
 
 def analisis():
     df = pd.read_csv('taylor_swift_spotify.csv')
-    albumes_a_incluir = ["Taylor Swift (Deluxe Edition)","Fearless (Taylor's Version)","Speak Now (Taylor's Version)", "Red (Taylor's Version","1989 (Taylor's Version) [Deluxe]","reputation","Lover","folklore (deluxe version)","evermore (deluxe version)","Midnights (The Til Dawn Edition)","THE TORTURED POETS DEPARTMENT: THE ANTHOLOGY"]
+    albs = ["Taylor Swift (Deluxe Edition)","Fearless (Taylor's Version)","Speak Now (Taylor's Version)", "Red (Taylor's Version","1989 (Taylor's Version) [Deluxe]","reputation","Lover","folklore (deluxe version)","evermore (deluxe version)","Midnights (The Til Dawn Edition)","THE TORTURED POETS DEPARTMENT: THE ANTHOLOGY"]
 
 # Filtrar el DataFrame para incluir solo esos álbumes
-    df_filtrado = df[df['album'].isin(albumes_a_incluir)]
+    fd = df[df['album'].isin(albs)]
 
 # Agrupar los datos para obtener el número de canciones por álbum
-    album_counts = df_filtrado.groupby('album').size().reset_index(name='canciones')
-    labels = album_counts['album'].tolist()
-    values = album_counts['canciones'].tolist()
-    options = {
+    cpa = fd.groupby('album').size().reset_index(name='canciones')
+    labels = cpa['album'].tolist()
+    values = cpa['canciones'].tolist()
+    grafikito = {
         "title": {"text": "Porcentaje de Canciones por Álbum", "left": "center"},
         "tooltip": {"trigger": "item"},
         "legend": {"orient": "vertical", "left": "left",},
         "series": [
             {
-                "name": "访问来源",
+                "name": "Álbum",
                 "type": "pie",
                 "radius": "50%",
                 "data": [{"value": v, "name": l} for v, l in zip(values, labels)],
@@ -33,8 +33,9 @@ def analisis():
                 },
             }
         ],
+        "color": ["beige", "blue", "green", "pink",'purple','khaki','fuchsia','yellow','orange','brown']
     }
     st_echarts(
-        options=options, height="600px",
+        grafikito=grafikito, height="600px",
     )
 
