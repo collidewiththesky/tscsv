ca23ab179309424b0df36730e649f97a297be440
Walala <conyclopez2@gmail.com>
2024-11-01 21:02:43 -0300

    Update pag3.py

--- pags/pag3.py
+++ pags/pag3.py
@@ -10,5 +10,5 @@ def analisis():
   cpa = ab['album'].value_counts()
   colores = ['yellow','red','green','pink','hotpink','khaki','blue','lightblue','orange','fuchsia']
   fig, ax = plt.subplots()
-  ax.pie(ab, labels=ab.index, autopct='%1.1f%%', startangle=45,colors=colores)
+  ax.pie(cpa, labels=cpa.index, autopct='%1.1f%%', startangle=45,colors=colores)
   st.pyplot(fig)
