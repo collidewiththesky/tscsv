0af81ebaf000bb5839cf643de59ce93b2d5d1fd6
Walala <conyclopez2@gmail.com>
2024-11-01 21:01:58 -0300

    Update pag3.py

--- pags/pag3.py
+++ pags/pag3.py
@@ -7,7 +7,7 @@ def analisis():
   st.title('Número de Canciones por Álbum')
   albs = ["Taylor Swift (Deluxe Edition)","Fearless (Taylor's Version)","Speak Now (Taylor's Version)", "Red (Taylor's Version","1989 (Taylor's Version) [Deluxe]","reputation","Lover","folklore (deluxe version)","evermore (deluxe version)","Midnights (The Til Dawn Edition)","THE TORTURED POETS DEPARTMENT: THE ANTHOLOGY"]
   ab = df[df['album'].isin(albs)]
-  cpa = ab.value_counts()
+  cpa = ab['album'].value_counts()
   colores = ['yellow','red','green','pink','hotpink','khaki','blue','lightblue','orange','fuchsia']
   fig, ax = plt.subplots()
   ax.pie(ab, labels=ab.index, autopct='%1.1f%%', startangle=45,colors=colores)
