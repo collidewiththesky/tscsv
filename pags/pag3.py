232e2d705ee3606d658e79d082736a7f1856e93d
Walala <conyclopez2@gmail.com>
2024-11-01 21:17:56 -0300

    Update pag3.py

--- pags/pag3.py
+++ pags/pag3.py
@@ -6,7 +6,7 @@ def analisis():
   df = pd.read_csv('taylor_swift_spotify.csv')
   albs = ["Taylor Swift (Deluxe Edition)","Fearless (Taylor's Version)","Speak Now (Taylor's Version)", "Red (Taylor's Version","1989 (Taylor's Version) [Deluxe]","reputation","Lover","folklore (deluxe version)","evermore (deluxe version)","Midnights (The Til Dawn Edition)","THE TORTURED POETS DEPARTMENT: THE ANTHOLOGY"]
   ab = df[df['album'].isin(albs)]
-  cpa = ab['album'].value_counts()
+  cpa = ab['album'].value_counts().reset_index()
   colores = ['yellow','red','green','lightskyblue','mediumpurple','hotpink','peru','silver','dimgray','limegreen']
   st.title('Porcentaje de Canciones por Álbumes')
 
