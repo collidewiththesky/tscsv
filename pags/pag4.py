21a0ac1acc738d78b6f045311edc2fab8c80e449
Walala <conyclopez2@gmail.com>
2024-11-04 21:40:18 -0300

    Update pag4.py

--- pags/pag4.py
+++ pags/pag4.py
@@ -1,1 +1,7 @@
+import pandas as pd
+import streamlit as st
 
+def recs():
+  st.title('Recomendaciones')
+  st.write('Este es un sistema de recomendaciones de canciones. Puedes seleccionar entre varias opciones para encontrar alguna canción que se ajuste a tí, por ejemplo, canciones bailables, versiones en vivo, acústicas, etc. ¡Elige lo que más se acomode a lo que deseas!')
+  st.radio('Quiero una canción...',['similar a una que ya conozco','de un estilo específico']
