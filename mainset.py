d686414aed3144caba7763e593a65f706b49f4a0
Walala <conyclopez2@gmail.com>
2024-11-05 08:28:01 -0300

    Update mainset.py

--- mainset.py
+++ mainset.py
@@ -2,7 +2,7 @@ import streamlit as st
 import requests
 from pags import homepage, pag2, pag3, pag4
 
-elegir = st.sidebar.radio('Ir a:',['Inicio','Álbumes','Análisis','Recomendaciones'])
+elegir = st.sidebar.radio('Ir a:',['Inicio','Álbumes','Análisis'])
 st.sidebar.markdown('![Alt Text](https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExdW92a242d3AxdHZyOHpwejR1Y21mb29xMWVxdThscjNmYW42d2E5OSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/KBbeJb7fvMnlJsfYWY/giphy.gif)')
 
 if elegir == 'Inicio':
@@ -11,5 +11,3 @@ elif elegir == 'Álbumes':
   pag2.albums()
 elif elegir == 'Análisis':
   pag3.analisis()
-elif elegir == 'Recomendaciones':
-  pag4.recs()
